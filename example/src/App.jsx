37d4a1124a390e9a95a18f144d0144ead850e894
elsiddig <parsercoder@gmail.com>
2020-09-13 23:35:48 +0200

    upgrading library style

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,66 +1,132 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { EasyNotify } from 'react-easy-notify'
 // import { Props } from 'react-easy-notify/dist/types/default'
 import 'react-easy-notify/dist/index.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
 const options = {
   type: 'success',
   title: 'success',
   status: true,
-  timeout: 3000,
+  timeout: 500000,
   message: 'hi there, here is my awesome alert message',
   position: 'top-right'
 }
 
 const App = () => {
-  const [type, setType] = useState('success')
-  const [position, setPosition] = useState('top-right')
-  const [animationType, setAnimationType] = useState('pops-up')
-  const callNot = () => {
-    EasyNotify.notify({ ...options, type, position, animationType })
+  const position = (position) => {
+    EasyNotify.notify({ ...options, position })
   }
+  const type = (type) => {
+    EasyNotify.notify({ ...options, type })
+  }
+  const animation = (animationType) => {
+    EasyNotify.notify({ ...options, animationType })
+  }
+
   return (
-    <div>
-      <div
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: `translate(-50%,-50%)`
-        }}
-      >
-        <p>select alert options</p>
-        <select value={type} onChange={({ target }) => setType(target.value)}>
-          <option>Alert Type</option>
-          <option>success</option>
-          <option>warning</option>
-          <option>danger</option>
-        </select>
-        <select
-          value={position}
-          onChange={({ target }) => setPosition(target.value)}
-        >
-          <option>Alert Position</option>
-          <option>top-left</option>
-          <option>top-right</option>
-          <option>top-center</option>
-          <option>bottom-left</option>
-          <option>bottom-right</option>
-          <option>bottom-center</option>
-        </select>
-        <select
-          value={animationType}
-          onChange={({ target }) => setAnimationType(target.value)}
-        >
-          <option>Alert Animation Type</option>
-          <option>pops-up</option>
-          <option>fade-in</option>
-          <option>slide-left</option>
-          <option>slide-right</option>
-          <option>vibration</option>
-        </select>
-        <button onClick={callNot}>click me</button>
+    <div className='text-center my-container'>
+      <div className='wrapper'>
+        <div className='contents'>
+          <p>Choose Position</p>
+
+          <div>
+            <button
+              className='btn btn-primary m-1'
+              onClick={() => position('top-left')}
+            >
+              Top Left
+            </button>
+            <button
+              className='btn btn-primary m-1'
+              onClick={() => position('top-center')}
+            >
+              Top Center
+            </button>
+            <button
+              className='btn btn-primary m-1'
+              onClick={() => position('top-right')}
+            >
+              Top Right
+            </button>
+          </div>
+          <div>
+            <button
+              className='btn btn-primary m-1'
+              onClick={() => position('bottom-left')}
+            >
+              Bottom Left
+            </button>
+            <button
+              className='btn btn-primary m-1'
+              onClick={() => position('bottom-center')}
+            >
+              Bottom Center
+            </button>
+            <button
+              className='btn btn-primary m-1'
+              onClick={() => position('bottom-right')}
+            >
+              Bottom Right
+            </button>
+          </div>
+        </div>
+        {/* alert types*/}
+        <div className='contents'>
+          <p>Choose Alert Type</p>
+
+          <button
+            className='btn btn-success m-1'
+            onClick={() => type('success')}
+          >
+            Success
+          </button>
+          <button
+            className='btn btn-warning m-1'
+            onClick={() => type('warning')}
+          >
+            Warning
+          </button>
+          <button className='btn btn-danger m-1' onClick={() => type('danger')}>
+            Danger
+          </button>
+        </div>
+
+        {/* alert animation type */}
+        <div className='contents'>
+          <p>Choose Animation Type</p>
+
+          <button
+            className='btn btn-primary m-1'
+            onClick={() => animation('pops-up')}
+          >
+            Pops-up
+          </button>
+          <button
+            className='btn btn-primary m-1'
+            onClick={() => animation('fade-in')}
+          >
+            Fade-in
+          </button>
+          <button
+            className='btn btn-primary m-1'
+            onClick={() => animation('slide-left')}
+          >
+            Slide Left
+          </button>
+          <button
+            className='btn btn-primary m-1'
+            onClick={() => animation('slide-right')}
+          >
+            Slide Right
+          </button>
+          <button
+            className='btn btn-primary m-1'
+            onClick={() => animation('vibration')}
+          >
+            Vibration
+          </button>
+        </div>
       </div>
       <EasyNotify />
     </div>
